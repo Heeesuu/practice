a924a86e42ea23958d4d54035e749837051d0e70
Heeesuu <gmltn0508@icloud.com>
2023-05-25 11:51:29 +0900

    저장 처리

--- src/main/java/com/ll/codicaster/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/ll/codicaster/boundedContext/article/controller/ArticleController.java
@@ -1,15 +1,32 @@
 package com.ll.codicaster.boundedContext.article.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ll.codicaster.boundedContext.article.entity.Article;
+import com.ll.codicaster.boundedContext.article.service.ArticleService;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
 
+	@Autowired
+	private ArticleService articleService;
+
 	@GetMapping("/write")
 	public String articleWrite() {
 		return "usr/article/write";
 	}
+
+	@PostMapping("/write")
+	public String articleWriteSave(Article article) {
+
+		articleService.write(article);
+
+		return "redirect:/article/list";
+	}
+
 }
--- src/main/java/com/ll/codicaster/boundedContext/article/service/ArticleService.java
+++ src/main/java/com/ll/codicaster/boundedContext/article/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.ll.codicaster.boundedContext.article.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ll.codicaster.boundedContext.article.entity.Article;
 import com.ll.codicaster.boundedContext.article.repository.ArticleRepository;
 
 @Service
@@ -11,7 +12,9 @@ public class ArticleService {
 	@Autowired
 	private ArticleRepository articleRepository;
 
-	public void write(){
+	public void write(Article article) {
 
+		articleRepository.save(article);
 	}
+
 }
